3080e03c6f971d14ef3e07b9b160aae1ffc3f54f
Hunter2454254 <126Hhaynes@frhsd.com>
2025-03-17 13:49:42 -0400

    done

--- js/index.js
+++ js/index.js
@@ -1,29 +1,56 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
-
-window.onresize = function() {
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+window.onresize = function () {
     canvas.width = innerWidth;
     canvas.height = innerHeight;
 }
 
 class Boundary {
-    constructor({position}) {
-    this.position = position
-    this.width = 40
-    this.height = 40
-}
-
-draw() {
-    c.fillStyle = 'blue'
-    c.fillRect(this.position.x, this.position.y, this.width, this.height)
-}
-}
-
-const boundary = new Boundary ({
-    position: {
-        x: 0,
-        y: 0
+    static width = 40;
+    static height = 40;
+    constructor({ position }) {
+        this.position = position
+        this.width = 40
+        this.height = 40
     }
+
+    draw() {
+        c.fillStyle = 'blue'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+const map = [
+    ['-', '-', '-', '-', '-', '-',],
+    ['-', ' ', ' ', ' ', ' ', '-',],
+    ['-', ' ', '-', '-', ' ', '-',],
+    ['-', ' ', ' ', ' ', ' ', '-',],
+    ['-', '-', '-', '-', '-', '-',]
+]
+
+
+const boundaries = []
+map.forEach((row, i) => {
+    row.forEach((symbol, j) => {
+        switch (symbol) {
+            case '-':
+                boundaries.push(
+                    new Boundary({
+                        position: {
+                            x: Boundary.width * j,
+                            y: Boundary.height * i
+                        }
+                    })
+                )
+                break
+        }
+    })
 })
 
-boundary.draw()
+
+
+
+boundaries.forEach((boundary) => {
+    boundary.draw()
+})
